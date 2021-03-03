308717d7d0aacb68484eac4bc34b79a572051e97
marcin wojtyczka <marcin.wojtyczka@alexanderthamm.com>
2021-03-03 19:34:38 +0100

    added additional worker policies var

--- output.tf
+++ output.tf
@@ -2,3 +2,10 @@ output argocd_state {
   value = module.argocd.state
 }
 
+output "vpc_id" {
+  value = module.network.vpc_id
+}
+
+output "private_subnets" {
+  value = module.network.private_subnets
+}
