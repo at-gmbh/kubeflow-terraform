1c55e7dba5066973640a2379926767c8639c65bc
Jürgen Stary <juergen.stary@alexanderthamm.com>
2021-03-13 13:35:56 +0100

    Poddefault - Secretinjection

--- main.tf
+++ main.tf
@@ -315,6 +315,7 @@ module profiles {
 module kubeflow-pod-default {
   source = "git::https://github.com/at-gmbh/swiss-army-kube.git//modules/kubeflow-pod-default?ref=feature/pod_defaults"
   external_secrets_deployment_role_arn = local.external_secrets_deployment_role_arn
+  cluster_name = var.cluster_name
 }
 
 // Create YAML specs for kube2iam
